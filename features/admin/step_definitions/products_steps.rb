7a1dd0b38332c325c032f60300b291a806e8127f
Eric Chiang <Eric.Ti.Yu.Chiang@gmail.com>
2013-12-10 07:32:53 -0500

    product feature login as admin step

--- features/admin/step_definitions/products_steps.rb
+++ features/admin/step_definitions/products_steps.rb
@@ -1,6 +1,10 @@
 Given(/^I am logged in as an admin$/) do
   @admin = FactoryGirl.create(:admin)
-    pending # express the regexp above with the code you wish you had
+  visit new_user_session_path
+  fill_in('user_email', with: @admin.email)
+  fill_in('user_password', with: @admin.password)
+  click_button('Sign in')
+  expect(page).to have_content('Signed in successfully')
 end
 
 When(/^I visit the admin Menu page$/) do
