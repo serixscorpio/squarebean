62c186cb479c8e64f29deba660af05463214d9d8
Eric Chiang <Eric.Ti.Yu.Chiang@gmail.com>
2013-12-10 07:18:42 -0500

    verify 200 OK for About feature's step

--- features/step_definitions/about_steps.rb
+++ features/step_definitions/about_steps.rb
@@ -3,7 +3,6 @@ When(/^I visit the About page$/) do
 end
 
 Then(/^I should see a description about why Square face and why Bean face$/) do
-  page.should have_content("That is how we came to existence.")
-  page.should have_content("We are an exclusively online make-to-order virtual shop that specializes western desserts with Asian twists with an emphasis on chiffon cakes that are moist and light. Customization is one of the things that sets us apart from other dessert shops. Our flavor profiles allow each ingredient to shine, yet blens nicely with each other. Please inspire us with your favorite combination of flavors!")
+  expect(page.status_code).to be 200
 end
 
